72c0dc87082ff426c5688258eb62b55101cbc7ed
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-08-30 19:07:40 -0400

    Update config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "pri-red": "#7F1637",
+        "pri-yellow": "#FFC857",
+        "pri-brow": "#825C3A",
+      },
+      fontFamily: {
+        openSans: ["Open Sans", "sans-serif"],
+        playFair: ["Playfair Display", "serif"],
+      },
+    },
   },
   plugins: [],
 };
