1acde4ee26996b93b3d8972fc861700f358a4b5a
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-08-31 18:41:25 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         "pri-red": "#7F1637",
         "pri-yellow": "#FFC857",
         "pri-brow": "#825C3A",
+        "pri-blue": "#03071e"
       },
       fontFamily: {
         openSans: ["Open Sans", "sans-serif"],
