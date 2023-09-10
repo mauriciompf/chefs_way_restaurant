ed06f0e0da6168750080285dae109f0df0cd17e6
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-09-10 18:06:18 -0400

    Update app.ts

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,26 +1,2 @@
-import Swiper from "swiper";
-import { Navigation, Pagination, Autoplay, Keyboard } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-
-const swiper = new Swiper(".swiper", {
-  modules: [Navigation, Pagination, Autoplay, Keyboard],
-  centeredSlides: true,
-  direction: "horizontal",
-  loop: true,
-  keyboard: {
-    enabled: true,
-  },
-  autoplay: {
-    delay: 3500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+import heroCarousel from "./carousel.ts";
+heroCarousel;
