155a2a466976b1b3b04df83bf877dd6adae4a262
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-09-11 16:12:35 -0400

    Update app.ts

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,2 +1,8 @@
 import heroCarousel from "./carousel.ts";
 heroCarousel;
+
+import showBackButton from "./show-back-button.ts";
+showBackButton;
+
+import menuFilter from "./menu-filter.ts";
+menuFilter;
