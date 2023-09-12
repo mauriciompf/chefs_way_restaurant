bcaeb5e6ab03ab2f7472012aabf8c86c717671fc
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-09-11 20:28:43 -0400

    Update menu-filter.ts

--- src/ts/menu-filter.ts
+++ src/ts/menu-filter.ts
@@ -4,17 +4,18 @@ const items = document.querySelectorAll("[data-filter-item]");
 export default buttons.forEach((button) => {
   button.addEventListener("click", () => {
     buttons.forEach((btn) => {
-      btn.querySelector("span")!.classList.remove("bg-black");
+      btn.classList.remove("border-pri-yellow", "border-4");
+      btn.querySelector("span")!.classList.remove("text-pri-yellow");
     });
-
-    button.querySelector("span")?.classList.add("bg-black");
+    button.classList.add("border-pri-yellow", "border-4");
+    button.querySelector("span")?.classList.add("text-pri-yellow");
 
     const filterCategory = button.getAttribute("data-filter");
 
     items.forEach((item) => {
       const itemCategory = item.getAttribute("data-category");
 
-      if (filterCategory === "meat" || filterCategory === itemCategory) {
+      if (filterCategory === "all" || filterCategory === itemCategory) {
         item.classList.remove("hidden");
       } else {
         item.classList.add("hidden");
