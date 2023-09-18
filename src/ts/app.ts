23d5b54ef670a98648b8c781bcd8b44a2f483e74
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-09-17 20:35:04 -0400

    Update app.ts

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,7 +1,26 @@
-import "./hero-carousel.ts";
-import "./menu-filter.ts";
-import "./validate-email.ts";
-import "./gsap.ts";
-import "./cart-toggle.ts";
-import "./cart-system.ts";
-import "./plates.ts";
+// HEADER
+import "./header/header-anim.ts";
+
+// MAIN
+// HERO SECTION
+import "./main/hero_images/hero-images.ts";
+import "./main/hero_images/hero-carousel.ts";
+
+// MENU SECTION
+import "./main/menu/menu-filter.ts";
+import "./main/menu/menu-anim.ts";
+import "./main/menu/plates/plates.ts";
+import "./main/menu/plates/cart/cart-toggle.ts";
+
+// ABOUT US
+import "./main/about-us/about-us-anim.ts";
+
+// TESTIMONIALS SECTION
+import "./main/testimonials/testimonials.ts";
+import "./main/testimonials/testimonials-anim.ts";
+
+// EMAIL VALIDATION
+import "./main/validate-email.ts";
+
+// ASIDE
+import "./aside/aside-back-btn-anim.ts";
