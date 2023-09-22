7d666bdb415e085587698c9a8988ce29e5e742e3
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-09-21 21:08:55 -0400

    Update header-anim.ts

--- src/ts/header/header-anim.ts
+++ src/ts/header/header-anim.ts
@@ -3,10 +3,11 @@ import "../main/menu/plates/displayPlates.ts";
 // @ts-ignore
 let tl = gsap.timeline();
 
-// Header y position
+// Animate the ".header" element by moving it upward
+// This is for the initial header animation.
 tl.from(".header", { y: -100, duration: 1 });
 
-// Hiding header off-screen
+// Define a GSAP animation for hiding the header element off-screen.
 // @ts-ignore
 export const headerAnim = gsap
   .from(".header", {
@@ -16,7 +17,9 @@ export const headerAnim = gsap
   })
   .progress(1);
 
-// @ts-ignore
+// Create a ScrollTrigger to detect scrolling behavior.
+// When scrolling enters the viewport
+// @ts-ignore: Ignoring TypeScript check for ScrollTrigger usage.
 ScrollTrigger.create({
   start: "top top",
   end: 99999,
@@ -25,7 +28,9 @@ ScrollTrigger.create({
   },
 });
 
-// @ts-ignore
+// Create a ScrollTrigger to handle the "onEnter" event.
+// When the ".header" element enters the viewport
+// @ts-ignore: Ignoring TypeScript check for ScrollTrigger usage.
 ScrollTrigger.create({
   // @ts-ignore
   onEnter: () => gsap.to(".header", { height: "auto", duration: 0.2 }),
