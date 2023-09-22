69cc752440d8c9a2d44bcbbd953338f1e29ea883
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-09-21 21:09:34 -0400

    Update cart-toggle.ts

--- src/ts/main/menu/plates/cart/cart-toggle.ts
+++ src/ts/main/menu/plates/cart/cart-toggle.ts
@@ -1,21 +1,31 @@
-const contentCart = document.querySelector("#content-cart") as HTMLElement;
-const container = document.querySelector("#container") as HTMLElement;
+const contentCart: HTMLElement | null = document.querySelector("#content-cart");
+const container: HTMLElement | null = document.querySelector("#container");
 
-function openCart() {
-  contentCart.style.width = "350px";
-  container.style.marginRight = "350px";
-  contentCart.classList.add("border-l-2");
+function openCart(): void {
+  if (contentCart && container) {
+    contentCart.style.width = "350px";
+    container.style.marginRight = "350px";
+    contentCart.classList.add("border-l-2");
+  }
 }
 
-function closeCart() {
-  contentCart.style.width = "0";
-  container.style.marginRight = "0";
-  contentCart.classList.remove("border-l-2");
+function closeCart(): void {
+  if (contentCart && container) {
+    contentCart.style.width = "0";
+    container.style.marginRight = "0";
+    contentCart.classList.remove("border-l-2");
+  }
 }
 
-export default (() => {
-  document.querySelector("#open-cart-btn")?.addEventListener("click", openCart);
-  document
-    .querySelector("#close-cart-btn")
-    ?.addEventListener("click", closeCart);
+export default ((): void => {
+  const openCartBtn = document.querySelector("#open-cart-btn");
+  const closeCartBtn = document.querySelector("#close-cart-btn");
+
+  if (openCartBtn) {
+    openCartBtn.addEventListener("click", openCart);
+  }
+
+  if (closeCartBtn) {
+    closeCartBtn.addEventListener("click", closeCart);
+  }
 })();
