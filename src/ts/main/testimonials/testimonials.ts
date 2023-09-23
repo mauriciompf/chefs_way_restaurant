ac5dc1fef18b65dc82e4bc4cadb86c12fd53593b
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-09-22 20:15:21 -0400

    Update testimonials.ts

--- src/ts/main/testimonials/testimonials.ts
+++ src/ts/main/testimonials/testimonials.ts
@@ -1,53 +1,86 @@
 import testimonials from "./testimonials-data.json";
 
-function displayStars(stars: number) {
-  let listStars: string[] = [];
-  for (let i = 0; i < stars; i++) {
-    listStars.push(`<i class="fa-solid fa-star"></i>`);
-  }
-  return listStars.join("");
+interface TestimonialData {
+  id: number;
+  image: string;
+  stars: number;
+  name: string;
+  description: string;
 }
 
-export default (() => {
+function displayStars(stars: number): DocumentFragment {
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < stars; i++) {
+    const starIcon = document.createElement("i");
+    starIcon.className = "fa-solid fa-star";
+    fragment.appendChild(starIcon);
+  }
+
+  return fragment;
+}
+
+export default ((): void => {
   const listTestimonials = document.querySelector(
     "#list-testimonials"
   ) as HTMLElement;
 
-  interface TestimonialData {
-    id: number;
-    image: string;
-    stars: number;
-    name: string;
-    description: string;
-  }
-
   if (testimonials != null) {
     (testimonials as TestimonialData[]).forEach((testimonial) => {
       const newTestimonial = document.createElement("figure");
-      newTestimonial.setAttribute(`data-testimonials-${testimonial.id}`, "");
-      newTestimonial.setAttribute(
-        "class",
-        "shadow-2xl border-4 border-pri-yellow grid gap-4 rounded-lg py-4"
+      newTestimonial.dataset[`testimonials-${testimonial.id}`] = "";
+      newTestimonial.classList.add(
+        "shadow-2xl",
+        "border-4",
+        "border-pri-yellow",
+        "grid",
+        "gap-4",
+        "rounded-lg",
+        "py-4"
       );
 
-      newTestimonial.innerHTML = `
-        <div class="grid place-items-center">
-            <img
-            class="w-[20%] rounded-full"
-            src="${testimonial.image}"
-            alt="${testimonial.name}"
-            />
-        </div>
-        <figcaption class="text-center">
-            <span class="text-xl font-bold">${testimonial.name}</span>
-            <div class="text-yellow-300 flex gap-4 justify-center mt-4">
-                ${displayStars(testimonial.stars)}
-            </div>
-        </figcaption>
-        <blockquote class="text-center mx-auto text-gray-600 w-[90%] text-base">${
-          testimonial.description
-        }</blockquote>
-        `;
+      const gridDiv = document.createElement("div");
+      gridDiv.classList.add("grid", "place-items-center");
+
+      const img = document.createElement("img");
+      img.className = "w-[20%] rounded-full";
+      img.src = testimonial.image;
+      img.alt = testimonial.name;
+
+      gridDiv.appendChild(img);
+
+      const figcaption = document.createElement("figcaption");
+      const nameSpan = document.createElement("span");
+      figcaption.classList.add("text-center");
+      nameSpan.classList.add("text-xl", "font-bold");
+      nameSpan.textContent = testimonial.name;
+
+      const starsDiv = document.createElement("div");
+      starsDiv.classList.add(
+        "text-yellow-300",
+        "flex",
+        "gap-4",
+        "justify-center",
+        "mt-4"
+      );
+      starsDiv.appendChild(displayStars(testimonial.stars));
+
+      figcaption.appendChild(nameSpan);
+      figcaption.appendChild(starsDiv);
+
+      const blockquote = document.createElement("blockquote");
+      blockquote.classList.add(
+        "text-center",
+        "mx-auto",
+        "text-gray-600",
+        "w-[90%]",
+        "text-base"
+      );
+      blockquote.textContent = testimonial.description;
+
+      newTestimonial.appendChild(gridDiv);
+      newTestimonial.appendChild(figcaption);
+      newTestimonial.appendChild(blockquote);
 
       listTestimonials.appendChild(newTestimonial);
     });
