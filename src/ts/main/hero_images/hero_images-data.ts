b8dda5936d96eca3577f2a805f8ac58a2a3c6295
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-09-24 17:27:31 -0400

    Add .ts  data, remove .json

---
+++ src/ts/main/hero_images/hero_images-data.ts
@@ -0,0 +1,43 @@
+import imgUrl1 from "../../../images/food-hero_1.svg";
+import imgUrl2 from "../../../images/food-hero_2.jpg";
+import imgUrl3 from "../../../images/food-hero_3.jpg";
+import imgUrl4 from "../../../images/food-hero_4.jpg";
+import imgUrl5 from "../../../images/food-hero_5.jpg";
+
+export default [
+  {
+    id: 1,
+    name: "Spicy Thai Green Curry",
+    description:
+      "A bowl of Spicy Thai Green Curry with tender chicken, green vegetables, and fragrant jasmine rice, all in a rich, aromatic green curry sauce.",
+    image: imgUrl1,
+  },
+  {
+    id: 2,
+    name: "Strawberry Shortcake",
+    description:
+      "Delight in layers of sponge cake, fresh strawberries, and velvety whipped cream, garnished with more berries and a sprinkle of powdered sugar.",
+    image: imgUrl2,
+  },
+  {
+    id: 3,
+    name: "Sushi Platter",
+    description:
+      "A colorful assortment of sushi rolls and sashimi, filled with fresh ingredients like fish, avocado, and cucumber, elegantly served with pickled ginger, wasabi, and soy sauce.",
+    image: imgUrl3,
+  },
+  {
+    id: 4,
+    name: "Classic Margherita Pizza",
+    description:
+      "Thin, crispy crust topped with ripe tomatoes, mozzarella cheese, and basil leaves, baked to perfection for a mouthwatering taste.",
+    image: imgUrl4,
+  },
+  {
+    id: 5,
+    name: "Chocolate Fondue Fountain",
+    description:
+      "Indulge in a decadent Chocolate Fondue Fountain surrounded by dippable treats like pineapple, strawberries, marshmallows, and pound cake cubes. A sweet delight for all.",
+    image: imgUrl5,
+  },
+];
---
+++ src/ts/main/menu/plates/plates-data.ts
@@ -0,0 +1,93 @@
+import imgUrl1 from "../../../../images/plate-1.svg";
+import imgUrl2 from "../../../../images/plate-2.svg";
+import imgUrl3 from "../../../../images/plate-3.svg";
+import imgUrl4 from "../../../../images/plate-burger.png";
+import imgUrl5 from "../../../../images/plate-pizza-1.png";
+import imgUrl6 from "../../../../images/plate-pizza-2.png";
+import imgUrl7 from "../../../../images/plate-pizza-3.png";
+import imgUrl8 from "../../../../images/plate-drink.png";
+import imgUrl9 from "../../../../images/plate-dessert-1.png";
+import imgUrl10 from "../../../../images/plate-dessert-2.png";
+
+export default [
+  {
+    id: 1,
+    name: "Galactic Gourmet",
+    description: "Smoky BBQ Ribs: Tender, tangy, and irresistible.",
+    price: 12.99,
+    image: imgUrl1,
+    category: "meat",
+  },
+  {
+    id: 2,
+    name: "Rainbow Velvet Dream",
+    description: "Spicy Sausage Links: Fiery, flavorful, and sizzling.",
+    price: 9.99,
+    image: imgUrl2,
+    category: "meat",
+  },
+  {
+    id: 3,
+    name: "Enchanted Forest Fusion",
+    description: "Juicy Bacon Strips: Crispy, savory, and mouthwatering.",
+    price: 6.99,
+    image: imgUrl3,
+    category: "meat",
+  },
+  {
+    id: 4,
+    name: "Moonlight Mango Tango",
+    description: "Classic Cheeseburger: Juicy, cheesy, and satisfying.",
+    price: 3.99,
+    image: imgUrl4,
+    category: "burger",
+  },
+  {
+    id: 5,
+    name: "Celestial Crispy Nebula",
+    description: "Pepperoni Supreme: Cheesy, zesty, and loaded.",
+    price: 9.99,
+    image: imgUrl5,
+    category: "pizza",
+  },
+  {
+    id: 6,
+    name: "Oceanic Oasis",
+    description: "Veggie Delight: Fresh, colorful, and wholesome.",
+    price: 7.99,
+    image: imgUrl6,
+    category: "pizza",
+  },
+  {
+    id: 7,
+    name: "Mystical Midnight",
+    description: "Hawaiian Bliss: Sweet, savory, and tropical.",
+    price: 11.99,
+    image: imgUrl7,
+    category: "pizza",
+  },
+  {
+    id: 8,
+    name: "Sunshine Serenade",
+    description: "Tropical Paradise: Refreshing, fruity, and exotic.",
+    price: 3.99,
+    image: imgUrl8,
+    category: "drink",
+  },
+  {
+    id: 9,
+    name: "Whimsical Wonderland",
+    description: "Chocolate Decadence: Rich, velvety, and indulgent.",
+    price: 4.99,
+    image: imgUrl9,
+    category: "dessert",
+  },
+  {
+    id: 10,
+    name: "Electric Eel Euphoria",
+    description: "Berry Bliss Parfait: Sweet, creamy, and fruity.",
+    price: 3.99,
+    image: imgUrl10,
+    category: "dessert",
+  },
+];
